0028be9a5126197b713eefaf869ea18c7ede6dc4
Sam Robbins <samrobbinsgb@gmail.com>
2020-12-16 15:33:57 +0000

    Start working on a homepage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
 import { getHomepage } from "../lib/graphcms";
-
+import Head from "next/head";
 export default function Home({ homepage }) {
   const data = homepage[0];
+  console.log(data.socialLinks);
   return (
-    <div className="pt-6">
-      <h1 className="text-center text-4xl font-semibold">{data.name}</h1>
-    </div>
+    <>
+      <Head>
+        <title>Sam Robbins</title>
+      </Head>
+      <div className="pt-6 px-2">
+        <h1 className="text-center text-4xl font-semibold">{data.name}</h1>
+        <h2 className="text-lg text-gray-800 text-center">
+          {data.description}
+        </h2>
+        <div className="flex justify-center gap-x-4 pt-4">
+          {data.socialLinks.map((entry) => (
+            <a href={entry.link}>
+              <img
+                className="h-12 w-20"
+                src={entry.image.url}
+                alt={entry.name}
+              />
+            </a>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
