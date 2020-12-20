ab97097ee7a84719967f92c2dd5860d5661f8eaa
Sam Robbins <samrobbinsgb@gmail.com>
2020-12-20 17:38:34 +0000

    Add a fade

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -2,7 +2,12 @@ import { getPortfolios, getPortfolioCategories } from "@/lib/graphcms";
 import FilledNav from "@/components/fillednav";
 import Link from "next/link";
 import { useState } from "react";
-import { motion, AnimateSharedLayout, MotionConfig } from "framer-motion";
+import {
+  motion,
+  AnimateSharedLayout,
+  MotionConfig,
+  AnimatePresence,
+} from "framer-motion";
 export default function Portfolio({ portfolios, categories }) {
   const [category, setCategory] = useState(false);
   return (
@@ -48,25 +53,35 @@ export default function Portfolio({ portfolios, categories }) {
         </div>
         <div>
           <div class="flex flex-wrap container mx-auto justify-center py-4 px-4 gap-4">
-            {portfolios
-              .filter((item) =>
-                category ? item.categories.includes(category) : item
-              )
-              .map((item) => (
-                <Link href={"/portfolio/" + item.slug}>
-                  <a class="w-full sm:w-2/5 lg:w-1/5 border border-gray-300 rounded hover:shadow-lg">
-                    <img
-                      class="h-32 w-full object-contain p-4"
-                      src={item.coverImage.url}
-                    />
-                    <hr class="my-4" />
-                    <div class="px-4">
-                      <h2 class="font-semibold h-16">{item.title}</h2>
-                      <p class="text-gray-600 pb-4">{item.description}</p>
-                    </div>
-                  </a>
-                </Link>
-              ))}
+            <AnimatePresence exitBeforeEnter>
+              {portfolios
+                .filter((item) =>
+                  category ? item.categories.includes(category) : item
+                )
+                .map((item) => (
+                  <motion.div
+                    animate={{ opacity: 1 }}
+                    initial={{ opacity: 0 }}
+                    exit={{ opacity: 0 }}
+                    className="w-full sm:w-2/5 lg:w-1/5 border border-gray-300 rounded hover:shadow-lg"
+                    key={item.title}
+                  >
+                    <Link href={"/portfolio/" + item.slug}>
+                      <a>
+                        <img
+                          class="h-32 w-full object-contain p-4"
+                          src={item.coverImage.url}
+                        />
+                        <hr class="my-4" />
+                        <div class="px-4">
+                          <h2 class="font-semibold h-16">{item.title}</h2>
+                          <p class="text-gray-600 pb-4">{item.description}</p>
+                        </div>
+                      </a>
+                    </Link>
+                  </motion.div>
+                ))}
+            </AnimatePresence>
           </div>
         </div>
       </div>
