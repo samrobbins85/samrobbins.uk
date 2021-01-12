973c3f1fdc4e360ceff1238e621d49040f1653a4
Sam Robbins <samrobbinsgb@gmail.com>
2021-01-12 19:00:17 +0000

    Smaller image sizes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   images: {
     domains: ["media.graphcms.com", "github.com"],
+    deviceSizes: [50, 100, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   //   webpack: (config, { dev, isServer }) => {
 
