b6e198220b43b5b5a2566bc53aab08964bdeb40a
Sam Robbins <samrobbinsgb@gmail.com>
2021-02-15 10:36:39 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
     domains: ["media.graphcms.com", "github.com"],
     deviceSizes: [50, 100, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
+  future: {webpack5 : true},
   //   webpack: (config, { dev, isServer }) => {
 
 
