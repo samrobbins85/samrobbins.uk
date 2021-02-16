2c67a5cb297f139683f9c85e6ac12fa69d221455
Sam Robbins <samrobbinsgb@gmail.com>
2021-02-16 13:23:45 +0000

    Add analytics to document

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,21 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            defer
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon='{"token": "8348650db5154c01999667d263b7e6e1"}'
+          ></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
