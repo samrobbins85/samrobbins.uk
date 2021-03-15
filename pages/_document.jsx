2ee065969a35090e92e0d6e61602221d1c6be178
Sam Robbins <samrobbinsgb@gmail.com>
2021-03-15 12:04:47 +0000

    Update with spa option for analytics

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
           <script
             defer
             src="https://static.cloudflareinsights.com/beacon.min.js"
-            data-cf-beacon='{"token": "8348650db5154c01999667d263b7e6e1"}'
+            data-cf-beacon='{"token": "8348650db5154c01999667d263b7e6e1", "spa": true}'
           />
         </Head>
         <body>
