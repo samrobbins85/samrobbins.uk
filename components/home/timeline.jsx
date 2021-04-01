1b77c5ab15ffc992edde6f816c84934166765f5f
Sam Robbins <samrobbinsgb@gmail.com>
2021-04-01 15:30:18 +0100

    🎨 Move description component into timeline file

--- components/home/timeline.jsx
+++ components/home/timeline.jsx
@@ -1,5 +1,26 @@
 import TimelineIcon from "./timelineicon";
-import Description from "./description";
+
+function Description({ title, date, description }) {
+  return (
+    <>
+      <div>
+        <div className="flex justify-between">
+          <h3
+            className="font-medium "
+            dangerouslySetInnerHTML={{ __html: title.html }}
+          />
+          <p className="pl-4 text-sm tracking-wide text-gray-500 text-right">
+            {new Date(date).toLocaleString("en-gb", {
+              month: "long",
+              year: "numeric",
+            })}
+          </p>
+        </div>
+      </div>
+      <div className="text-gray-700">{description}</div>
+    </>
+  );
+}
 
 export default function TimeLineItem({ data }) {
   return (
