1bc8068948ed4d64cb61599e37cb46a373d2c014
Sam Robbins <samrobbinsgb@gmail.com>
2021-04-16 18:37:47 +0100

    🎨 Working from new structure, need to refactor

--- components/newnav.jsx
+++ components/newnav.jsx
@@ -12,19 +12,7 @@ import {
   HomeIcon,
 } from "@heroicons/react/outline";
 import { ChevronDownIcon } from "@heroicons/react/solid";
-
-const solutions = [
-  {
-    name: "Blog",
-    href: "#",
-    icon: NewspaperIcon,
-  },
-  {
-    name: "Essays",
-    href: "#",
-    icon: AcademicCapIcon,
-  },
-];
+import Link from "next/link";
 
 const items = [
   { name: "Home", href: "/", icon: HomeIcon },
@@ -44,19 +32,75 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-function MobileTitle(name, link, item) {
+function DesktopDropdown({ x }) {
   return (
-    <a
-      key={name}
-      href={link}
-      className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-    >
-      <item.icon
-        className="flex-shrink-0 h-6 w-6 text-indigo-600"
-        aria-hidden="true"
-      />
-      <span className="ml-3 text-base font-medium text-gray-900">{name}</span>
-    </a>
+    <Popover className="relative">
+      {({ open }) => (
+        <>
+          <Popover.Button
+            className={classNames(
+              open ? "text-gray-900" : "text-gray-500",
+              "group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            )}
+          >
+            <span>{x.name}</span>
+            <ChevronDownIcon
+              className={classNames(
+                open ? "text-gray-600" : "text-gray-400",
+                "ml-2 h-5 w-5 group-hover:text-gray-500"
+              )}
+              aria-hidden="true"
+            />
+          </Popover.Button>
+
+          <Transition
+            show={open}
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
+          >
+            <Popover.Panel
+              static
+              className="absolute z-50 -ml-4 mt-3 transform px-2 w-screen max-w-xs sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2"
+            >
+              <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                  {x.items.map((item) => (
+                    <Link key={item.name} href={item.href}>
+                      <a className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                        <item.icon
+                          className="flex-shrink-0 h-6 w-6 text-indigo-600"
+                          aria-hidden="true"
+                        />
+                        <div className="ml-4">
+                          <p className="text-base font-medium text-gray-900">
+                            {item.name}
+                          </p>
+                        </div>
+                      </a>
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            </Popover.Panel>
+          </Transition>
+        </>
+      )}
+    </Popover>
+  );
+}
+
+function DesktopSingle({ x }) {
+  return (
+    <Link href={x.href}>
+      <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+        {x.name}
+      </a>
+    </Link>
   );
 }
 
@@ -74,90 +118,12 @@ export default function Example() {
                 </Popover.Button>
               </div>
               <Popover.Group as="nav" className="hidden md:flex space-x-10">
-                <a
-                  href="#"
-                  className="text-base font-medium text-gray-500 hover:text-gray-900"
-                >
-                  Home
-                </a>
-                <a
-                  href="#"
-                  className="text-base font-medium text-gray-500 hover:text-gray-900"
-                >
-                  About
-                </a>
-                <Popover className="relative">
-                  {({ open }) => (
-                    <>
-                      <Popover.Button
-                        className={classNames(
-                          open ? "text-gray-900" : "text-gray-500",
-                          "group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        )}
-                      >
-                        <span>Writing</span>
-                        <ChevronDownIcon
-                          className={classNames(
-                            open ? "text-gray-600" : "text-gray-400",
-                            "ml-2 h-5 w-5 group-hover:text-gray-500"
-                          )}
-                          aria-hidden="true"
-                        />
-                      </Popover.Button>
-
-                      <Transition
-                        show={open}
-                        as={Fragment}
-                        enter="transition ease-out duration-200"
-                        enterFrom="opacity-0 translate-y-1"
-                        enterTo="opacity-100 translate-y-0"
-                        leave="transition ease-in duration-150"
-                        leaveFrom="opacity-100 translate-y-0"
-                        leaveTo="opacity-0 translate-y-1"
-                      >
-                        <Popover.Panel
-                          static
-                          className="absolute z-50 -ml-4 mt-3 transform px-2 w-screen max-w-xs sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2"
-                        >
-                          <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                            <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                              {solutions.map((item) => (
-                                <a
-                                  key={item.name}
-                                  href={item.href}
-                                  className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
-                                >
-                                  <item.icon
-                                    className="flex-shrink-0 h-6 w-6 text-indigo-600"
-                                    aria-hidden="true"
-                                  />
-                                  <div className="ml-4">
-                                    <p className="text-base font-medium text-gray-900">
-                                      {item.name}
-                                    </p>
-                                  </div>
-                                </a>
-                              ))}
-                            </div>
-                          </div>
-                        </Popover.Panel>
-                      </Transition>
-                    </>
-                  )}
-                </Popover>
-
-                <a
-                  href="#"
-                  className="text-base font-medium text-gray-500 hover:text-gray-900"
-                >
-                  Portfolio
-                </a>
-                <a
-                  href="#"
-                  className="text-base font-medium text-gray-500 hover:text-gray-900"
-                >
-                  Snippets
-                </a>
+                {items.map((x) => {
+                  if ("items" in x) {
+                    return <DesktopDropdown x={x} />;
+                  }
+                  return <DesktopSingle x={x} />;
+                })}
               </Popover.Group>
             </div>
           </div>
@@ -189,57 +155,36 @@ export default function Example() {
                   </div>
                   <div className="mt-6">
                     <nav className="grid gap-y-8">
-                      <a
-                        href="/about"
-                        className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                      >
-                        <UserIcon
-                          className="flex-shrink-0 h-6 w-6 text-indigo-600"
-                          aria-hidden="true"
-                        />
-                        <span className="ml-3 text-base font-medium text-gray-900">
-                          About
-                        </span>
-                      </a>
-                      {solutions.map((item) => (
-                        <a
-                          key={item.name}
-                          href={item.href}
-                          className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                        >
-                          <item.icon
-                            className="flex-shrink-0 h-6 w-6 text-indigo-600"
-                            aria-hidden="true"
-                          />
-                          <span className="ml-3 text-base font-medium text-gray-900">
-                            {item.name}
-                          </span>
-                        </a>
-                      ))}
-                      <a
-                        href="/about"
-                        className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                      >
-                        <CollectionIcon
-                          className="flex-shrink-0 h-6 w-6 text-indigo-600"
-                          aria-hidden="true"
-                        />
-                        <span className="ml-3 text-base font-medium text-gray-900">
-                          Portfolio
-                        </span>
-                      </a>
-                      <a
-                        href="/about"
-                        className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                      >
-                        <CodeIcon
-                          className="flex-shrink-0 h-6 w-6 text-indigo-600"
-                          aria-hidden="true"
-                        />
-                        <span className="ml-3 text-base font-medium text-gray-900">
-                          Snippets
-                        </span>
-                      </a>
+                      {items.map((item) => {
+                        if ("items" in item) {
+                          return item.items.map((x) => (
+                            <Link key={x.name} href={x.href}>
+                              <a className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                                <x.icon
+                                  className="flex-shrink-0 h-6 w-6 text-indigo-600"
+                                  aria-hidden="true"
+                                />
+                                <span className="ml-3 text-base font-medium text-gray-900">
+                                  {x.name}
+                                </span>
+                              </a>
+                            </Link>
+                          ));
+                        }
+                        return (
+                          <Link key={item.name} href={item.href}>
+                            <a className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                              <item.icon
+                                className="flex-shrink-0 h-6 w-6 text-indigo-600"
+                                aria-hidden="true"
+                              />
+                              <span className="ml-3 text-base font-medium text-gray-900">
+                                {item.name}
+                              </span>
+                            </a>
+                          </Link>
+                        );
+                      })}
                     </nav>
                   </div>
                 </div>
