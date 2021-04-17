f11d900f517c3f1504c070693991c9a244a5b8ee
Sam Robbins <samrobbinsgb@gmail.com>
2021-04-17 10:31:58 +0100

    💄 Mobile navbar has x inline

--- components/newnav.jsx
+++ components/newnav.jsx
@@ -189,7 +189,7 @@ export default function Nav() {
               className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
             >
               <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-                <div className="pt-5 pb-6 px-5">
+                <div className="pt-5 pb-6 px-5 flex justify-between flex-row-reverse">
                   <div className="flex justify-end">
                     <div className="-mr-2">
                       <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-500">
@@ -198,7 +198,7 @@ export default function Nav() {
                       </Popover.Button>
                     </div>
                   </div>
-                  <div className="mt-6">
+                  <div className="mt-6 flex-grow pr-8">
                     <nav className="grid gap-y-8">
                       {items.map((item) => {
                         if ("items" in item) {
