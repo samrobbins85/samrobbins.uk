73d1ae5b4c59bbadfa94f27e8251835462a37d5b
Sam Robbins <samrobbinsgb@gmail.com>
2021-05-13 18:23:51 +0100

    ✨ Card of information

--- pages/oss.jsx
+++ pages/oss.jsx
@@ -24,9 +24,17 @@ export default function Home({ prs }) {
       <main className="py-6 px-4 max-w-85ch mx-auto">
         <h1 className="text-5xl font-semibold pb-4 text-center">Open Source</h1>
         <div className="grid gap-y-2">
-          {prs.map((x) => (
-            <div className="grid bg-gray-100 py-2 px-2">
-              <h2 className="font-semibold">{x.title}</h2>
+          {prs.map((pr) => (
+            <div className="flex gap-4 bg-gray-100 py-2 px-2">
+              <img
+                src={pr.repository.owner.avatarUrl}
+                alt={pr.repository.owner.name}
+                className="h-12 w-12"
+              />
+              <div>
+                <h2 className="font-semibold">{pr.title}</h2>
+                <p>{pr.repository.name}</p>
+              </div>
             </div>
           ))}
         </div>
