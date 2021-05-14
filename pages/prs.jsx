6637128874b38ed12732fb6aed225141193a0247
Sam Robbins <samrobbinsgb@gmail.com>
2021-05-14 17:34:23 +0100

    ✨ Add to Navbar and tidy up

--- pages/prs.jsx
+++ pages/prs.jsx
@@ -3,18 +3,17 @@ import Nav from "@/components/newnav";
 import getSearch from "@/lib/githubapi";
 
 export default function Home({ prs }) {
-  console.log(prs);
   return (
     <>
       <Head>
-        <title>About | Sam Robbins</title>
+        <title>Pull Requests | Sam Robbins</title>
         <meta
           property="og:image"
           content={`https://og.csnotes.me/**${escape(
             "Sam Robbins"
           )}**.png?theme=dark&md=1&fontSize=100px`}
         />
-        <meta property="og:title" content="Sam Robbins" />
+        <meta property="og:title" content="Pull Requests | Sam Robbins" />
         <meta
           property="og:description"
           content="The personal website of Sam Robbins"
@@ -22,18 +21,39 @@ export default function Home({ prs }) {
       </Head>
       <Nav />
       <main className="py-6 px-4 max-w-85ch mx-auto">
-        <h1 className="text-5xl font-semibold pb-4 text-center">Open Source</h1>
-        <div className="grid gap-y-2">
+        <h1 className="text-5xl font-semibold pb-6 text-center">
+          Pull Requests
+        </h1>
+        <div className="grid gap-y-4">
           {prs.map((pr) => (
-            <div className="flex gap-4 bg-gray-100 py-2 px-2">
+            <div className="flex gap-4 bg-nord-6 dark:bg-nord-0  py-4 px-4">
               <img
                 src={pr.repository.owner.avatarUrl}
                 alt={pr.repository.owner.name}
                 className="h-12 w-12"
               />
-              <div>
-                <h2 className="font-semibold">{pr.title}</h2>
-                <p>{pr.repository.name}</p>
+              <div className="flex justify-between flex-grow flex-wrap gap-x-2">
+                <div>
+                  <a
+                    href={pr.permalink}
+                    className="text-black hover:text-blue-600 dark:text-nord-6 dark:hover:text-blue-400"
+                  >
+                    <h2 className="font-semibold">{pr.title}</h2>
+                  </a>
+                  <a
+                    className="text-nord-3 hover:text-blue-600 dark:text-nord-4 dark:hover:text-blue-400"
+                    href={pr.repository.url}
+                  >
+                    {pr.repository.owner.login}/{pr.repository.name}
+                  </a>
+                </div>
+                <p className="text-nord-3">
+                  {new Date(pr.mergedAt).toLocaleString("en-gb", {
+                    day: "numeric",
+                    month: "long",
+                    year: "numeric",
+                  })}
+                </p>
               </div>
             </div>
           ))}
