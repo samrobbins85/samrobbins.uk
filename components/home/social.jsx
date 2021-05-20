ae2791700ee09180ee1fc936d67749e9b96baa8c
Sam Robbins <samrobbinsgb@gmail.com>
2021-05-20 17:03:51 +0100

    🐛 Highlight social in dark mode

--- components/home/social.jsx
+++ components/home/social.jsx
@@ -10,32 +10,32 @@ export default function SocialLinks() {
       name: "GitHub",
       icon: MarkGithubIcon,
       link: "https://github.com/samrobbins85",
-      className: "hover:text-black focus:text-black",
+      className: "hover:!text-black focus:!text-black",
       modifier: "w-6",
     },
     {
       name: "npm",
       icon: Npm,
       link: "https://www.npmjs.com/~samrobbins",
-      className: "hover:text-npm focus:text-npm",
+      className: "hover:!text-npm focus:!text-npm",
     },
     {
       name: "LinkedIn",
       icon: Linkedin,
       link: "https://www.linkedin.com/in/sam-robbins-gb/",
-      className: "hover:text-linkedin focus:text-linkedin",
+      className: "hover:!text-linkedin focus:!text-linkedin",
     },
     {
       name: "Twitter",
       icon: Twitter,
       link: "https://twitter.com/samrobbins85",
-      className: "hover:text-twitter focus:text-twitter",
+      className: "hover:!text-twitter focus:!text-twitter",
     },
     {
       name: "Unsplash",
       icon: Unsplash,
       link: "https://unsplash.com/@samrobbins",
-      className: "hover:text-black focus:text-black",
+      className: "hover:!text-black focus:!text-black",
     },
   ];
   return links.map((link) => (
