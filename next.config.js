01e35afd295cfaef0f5c368d1b855345ad501e31
Sam Robbins <samrobbinsgb@gmail.com>
2021-06-15 17:34:51 +0100

    ⬆️ Upgrade to Next.js 11

--- next.config.js
+++ next.config.js
@@ -7,7 +7,8 @@ module.exports = withBundleAnalyzer({
     domains: ["media.graphcms.com", "github.com"],
     deviceSizes: [50, 100, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
-  future: {
-    webpack5: true,
+  webpack5: true,
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 });
