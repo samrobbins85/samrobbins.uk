772ed4e3e9f135c14bfdf4fc4a95ee7fff073ccb
Sam Robbins <samrobbinsgb@gmail.com>
2021-08-01 11:41:19 +0100

    💄 Add more complete favicon set

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,10 @@ export default class MyDocument extends Document {
             crossOrigin="anonymous"
           />
           <link rel="icon" href="/favicon.ico" sizes="any" />
-          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <link rel="manifest" href="/manifest.webmanifest" />
+
           <link
             rel="alternate"
             type="application/rss+xml"
