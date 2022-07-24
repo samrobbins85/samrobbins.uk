3acbe0d304c0a1d46312aeb0ff3816ddc10d252f
Sam Robbins <samrobbinsgb@gmail.com>
2022-07-24 18:45:50 +0100

    Tidy up unused divs

--- components/projects/ProjectGrid.tsx
+++ components/projects/ProjectGrid.tsx
@@ -22,28 +22,26 @@ export default function ProjectGrid({
           category !== "All" ? item.categories.includes(category) : item
         )
         .map((x) => (
-          <div className="w-full sm:w-2/5 lg:w-1/4" key={x.title}>
-            <Link href={`/projects/${x.slug}`}>
-              <a className="group focus:outline-none">
-                <div className="px-2 py-2 rounded h-full border-2 group-focus:border-radix-slate7 border-transparent group-hover:bg-radix-slate2 flex items-center">
-                  <div className="flex items-center gap-x-4 ">
-                    <div className="h-8 w-8 shrink-0">
-                      <Icon
-                        icon={x.icon}
-                        className="h-8 w-8 text-radix-slate9 grayscale"
-                      />
-                    </div>
-                    <div className="flex flex-col">
-                      <span className="w-min-sm">{x.title}</span>
-                      <span className="text-radix-slate11 text-sm">
-                        {x.description}
-                      </span>
-                    </div>
+          <Link href={`/projects/${x.slug}`} key={x.title}>
+            <a className="group focus:outline-none w-full sm:w-2/5 lg:w-1/4">
+              <article className="px-2 py-2 rounded h-full border-2 group-focus:border-radix-slate7 border-transparent group-hover:bg-radix-slate2 flex items-center">
+                <div className="flex items-center gap-x-4 ">
+                  <div className="h-8 w-8 shrink-0">
+                    <Icon
+                      icon={x.icon}
+                      className="h-8 w-8 text-radix-slate9 grayscale"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <h2 className="w-min-sm">{x.title}</h2>
+                    <span className="text-radix-slate11 text-sm">
+                      {x.description}
+                    </span>
                   </div>
                 </div>
-              </a>
-            </Link>
-          </div>
+              </article>
+            </a>
+          </Link>
         ))}
     </div>
   );
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -14,32 +14,28 @@ export default function Blog({
 
       <div className="grid">
         {blogs.map((item) => (
-          <div className="py-4" key={item.title}>
-            <div>
-              <Link href={`/blog/${item.slug}`}>
-                <a>
-                  <h2 className="font-semibold text-xl sm:text-2xl text-radix-cyan11">
-                    {item.title}
-                  </h2>
-                </a>
-              </Link>
-              <div className="py-1">
-                <time
-                  className="text-radix-slate11 text-sm uppercase"
-                  dateTime={item.date}
-                >
-                  {new Date(item.date).toLocaleString("en-gb", {
-                    day: "numeric",
-                    month: "short",
-                    year: "numeric",
-                  })}
-                </time>
-              </div>
-              <p className="sm:text-lg text-radix-slate11">
-                {item.description}
-              </p>
+          <article className="py-4" key={item.title}>
+            <Link href={`/blog/${item.slug}`}>
+              <a>
+                <h2 className="font-semibold text-xl sm:text-2xl text-radix-cyan11">
+                  {item.title}
+                </h2>
+              </a>
+            </Link>
+            <div className="py-1">
+              <time
+                className="text-radix-slate11 text-sm uppercase"
+                dateTime={item.date}
+              >
+                {new Date(item.date).toLocaleString("en-gb", {
+                  day: "numeric",
+                  month: "short",
+                  year: "numeric",
+                })}
+              </time>
             </div>
-          </div>
+            <p className="sm:text-lg text-radix-slate11">{item.description}</p>
+          </article>
         ))}
       </div>
     </Layout>
