034680ed3f1216d39822ece8d4798bfc883407ab
Sam Robbins <samrobbinsgb@gmail.com>
2022-07-24 12:05:58 +0100

    Prevent shift when loading icons

--- components/projects/ProjectGrid.tsx
+++ components/projects/ProjectGrid.tsx
@@ -27,7 +27,7 @@ export default function ProjectGrid({
               <a className="group focus:outline-none">
                 <div className="px-2 py-2 rounded h-full border-2 group-focus:border-radix-slate7 border-transparent group-hover:bg-radix-slate2 flex items-center">
                   <div className="flex items-center gap-x-4 ">
-                    <div className="h-8 w-8">
+                    <div className="h-8 w-8 shrink-0">
                       <Icon
                         icon={x.icon}
                         className="h-8 w-8 text-radix-slate9 grayscale"
