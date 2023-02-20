243bcc69f8fc00195ae12ac9f670e0560d4fb03c
Sam Robbins <samrobbinsgb@gmail.com>
2023-02-20 19:46:02 +0000

    Finish upgrades

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,7 +1,7 @@
 import { ImageResponse } from "@vercel/og";
 import { NextRequest } from "next/server";
 export const config = {
-  runtime: "experimental-edge",
+  runtime: "edge",
 };
 
 const fontLight = fetch(
